1c2b647b18697dbff0e039beee569339281c25a6
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-05 12:07:43 +0500

    No need of id attribute
    Our GUI for the App Has been Completed. Now We Start Logic After This

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -4,12 +4,11 @@ export const TransactionHistory = () => {
   return (
     <>
       <h3>History</h3>
-      <ul id='list' className='list'>
-      <li className='minus'>
-      Cash <span>-$400</span><button className='delete-btn'></button>
-      </li>
+      <ul className='list'>
+        <li className='minus'>
+          Cash <span>-$400</span><button className='delete-btn'></button>
+        </li>
       </ul>
-
     </>
   )
 }
