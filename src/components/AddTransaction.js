cdd38874ec7a1e47364b2fc1779d3679184302b4
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-06 06:56:43 +0500

    Added UseState hook to the AddTransaction.Js
    We added UseState as we want to catch and update our amount and Description

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransaction = () => {
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
   return (
     <>
       <h3>Add New Transaction</h3>
       <form>
         <div className='form-control'>
-          <label htmlFor='text'>Text</label>
-          <input type='text' placeholder='Enter text...' />
+          <label htmlFor='text'>Description</label>
+          <input type='text' value={description} onChange={ (e) => setDescription(e.target.value)} placeholder='Enter text...' />
         </div>
         <div className='form-control'>
           <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type='number' placeholder='Enter Amount...' />
+          <input type='number' value={amount} onChange={ (e) => setAmount(e.target.value)} placeholder='Enter Amount...' />
         </div>
         <button className='btn'>Add Transaction</button>
       </form>
