76e565bf490ff6ca1dc731ca5359848a64516115
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-06 09:48:14 +0500

    Updated anded Transaction component to this

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,16 +1,14 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionHistory = () => {
-  const {transactions} = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
       <ul className='list'>
-      {transactions.map(transaction => (<li className='minus'>
-      {transaction.description} <span>-$400</span><button className='delete-btn'></button>
-    </li>) )}
-        
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
   )
