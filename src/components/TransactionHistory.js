18f4a0d1f49d80c5de06193fc470e43a52db6fa9
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-06 07:56:48 +0500

    Update TransactionHistory
    We are Using now GloabalContext

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionHistory = () => {
+  const {transactions} = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
       <ul className='list'>
-        <li className='minus'>
-          Cash <span>-$400</span><button className='delete-btn'></button>
-        </li>
+      {transactions.map(transaction => (<li className='minus'>
+      {transaction.description} <span>-$400</span><button className='delete-btn'></button>
+    </li>) )}
+        
       </ul>
     </>
   )
