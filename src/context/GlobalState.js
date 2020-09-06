d4f9bdb4921605d4f81b47ba5eb3c7a448367909
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-09-06 07:30:53 +0500

    Update Global State
    We created GlobalContext and a Providet

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,13 +1,31 @@
-import React, {createContext, useReducer} from 'react';
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 // Initial State
 
 const initialState = {
-    // Dummy Transactions Used at initial States
-    transactions: [
-        {id: 1, description: 'Flower', amount: -20},
-        {id: 2, description: 'Salary', amount: 300},
-        {id: 3, description: 'Book', amount: -10},
-        {id: 4, description: 'Camera', amount: -150}
-    ]
+  // Dummy Transactions Used at initial States
+  transactions: [
+    { id: 1, description: 'Flower', amount: -20 },
+    { id: 2, description: 'Salary', amount: 300 },
+    { id: 3, description: 'Book', amount: -10 },
+    { id: 4, description: 'Camera', amount: -150 }
+  ]
+};
+
+// Create Global Context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{
+      transactions: state.transactions
+    }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
